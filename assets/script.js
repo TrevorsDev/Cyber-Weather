46ee784c9e5564e1cf5aa59a292c4b6b0f3d3c39
Trevor Johnson <trevor9j@protonmail.com>
2024-02-05 23:18:38 -0700

    added psuedo code. Created 'current weather' section in JS

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 /*
 1) user inputs city into search bar
 2) user clicks submit button 
-3) event listener attached to submit button listens for a click to submit name
-4) we create a function that takes in city name and retrieves the lat and long from that city name
-5) 
+3) JS needs to be connected to users action of clicking HTML button (event listener)
+3) Event listener attached to submit button listens for a click of button to submit username
+4) We create a function that takes in the location of the users city name  
+5) We create a function that takes in the users city name and gathers data about that city
+6) Display users city data on current city temp
+7) Create function for current weather
+
 setparate functions for current weather and five day forecst*/
 // My API key: 38438435bd1e08c9d78e0ac7cd864567
 function getWeather(lat, lon) {
@@ -15,7 +19,9 @@ function getWeather(lat, lon) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            console.log("this here b tha data", data);
+            clearSubmition();
+            todayWeather(data);
         })
 }
 
@@ -39,6 +45,34 @@ function getLatAndLong(cityName) {
             const lon = data[0].lon
             getWeather(lat, lon);
         })
+}
+var currentWeather = document.querySelector("#current-weather");
+function todayWeather(data) {
+    var currentCity = document.createElement("h2");
+    //this line is gathering data from the api object in the console using the '.name' as a selector
+    currentCity.textContent = data.name;
+
+    var currentTemp = document.createElement("p");
+    //this line is gathering data from the api object in the console using the '.name' as a selector
+    currentTemp.textContent = `Temp:    ${data.main.temp} °`;
+
+    var currentWind = document.createElement("p");
+    //this line is gathering data from the api object in the console using the '.name' as a selector
+    currentWind.textContent = `Wind༄:    ${data.wind.speed}`;
+
+    var currentHumidity = document.createElement("p");
+    //this line is gathering data from the api object in the console using the '.name' as a selector
+    currentHumidity.textContent = 'Humidity ♨:'+'   '+data.main.humidity;
+
+    currentWeather.appendChild(currentCity);
+    currentWeather.appendChild(currentTemp);
+    currentWeather.appendChild(currentWind);
+    currentWeather.appendChild(currentHumidity);
+
+}
+
+function clearSubmition() {
+    currentWeather.innerHTML = "";
 }
 
 
