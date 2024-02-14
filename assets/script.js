ad0da29c46544189963adeca9c9e9cb0a2ce8961
Trevor Johnson <trevor9j@protonmail.com>
2024-02-13 18:25:46 -0700

    adding styling to the innerHTML elements in JS

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,10 @@
 3) Event listener attached to submit button listens for a click of button to submit username
 4) We create a function that takes in the location of the users city name  
 5) We create a function that takes in the users city name and gathers data about that city
-6) Display users city data on current city temp
+6) Display users city data on current city temp when "Enter" key or "Submit" button is clicked
 7) Create function for current weather
+8) Create Temp Wind and Humidity elements to display live on the current weather section
+9) How to create a 5-day forecast
 
 setparate functions for current weather and five day forecst*/
 // My API key: 38438435bd1e08c9d78e0ac7cd864567
@@ -15,7 +17,6 @@ function getWeather(lat, lon) {
 
     fetch(weatherURL)
         .then(function (response) {
-            console.log(response);
             return response.json();
         })
         .then(function (data) {
@@ -42,12 +43,6 @@ searchBox.addEventListener("keydown", function (event) {
     }
 });
 
-if (searchBox.value) {
-    console.log(searchBox.value);
-} else {
-    console.log('im not greater than 1');
-}
-
 function getLatAndLong(cityName) {
     console.log(cityName);
     const cityNameURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=38438435bd1e08c9d78e0ac7cd864567'
@@ -70,17 +65,27 @@ function todayWeather(data) {
     //this line is gathering data from the api object in the console using the '.name' as a selector
     currentCity.textContent = data.name;
 
+    // In the lines below, Im both creating innerHTML elements so that the current weather can be displayed, AND styling different elements separate colors
     var currentTemp = document.createElement("p");
     //this line is gathering data from the api object in the console using the '.name' as a selector
-    currentTemp.textContent = `Temp :    ${data.main.temp} °`;
+    var currentTimeLabel = document.createElement("span")
+    currentTimeLabel.textContent = `Temp : `;
+    currentTimeLabel.style.color = "#ff2a6d";
+    var currentTempSpan = document.createElement("span");
+    currentTempSpan.textContent = data.main.temp;
+    currentTempSpan.style.color = "#05d9e8";
+    var currentTimeDegree = document.createElement("span")
+    currentTimeDegree.textContent = ` °`;
+    // currentTemp.innerHTML = `Temp:   <span  class="temp-color-style">${data.main.temp}</span> °`
+    currentTemp.append(currentTimeLabel, currentTempSpan, currentTimeDegree)
 
     var currentWind = document.createElement("p");
     //this line is gathering data from the api object in the console using the '.name' as a selector
-    currentWind.textContent = `Wind༄ :    ${data.wind.speed}`;
+    currentWind.textContent = `Wind༄ :    ${data.wind.speed} mph`;
 
     var currentHumidity = document.createElement("p");
     //this line is gathering data from the api object in the console using the '.name' as a selector
-    currentHumidity.textContent = 'Humidity ♨ :' + '   ' + data.main.humidity;
+    currentHumidity.textContent = 'Humidity ♨ :' + '   ' + data.main.humidity + ' %';
 
     currentWeather.appendChild(currentCity);
     currentWeather.appendChild(currentTemp);
