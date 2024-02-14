5d2ef7ed4c9ffbfa726f3b58290f53b40c4e5471
Trevor Johnson <trevor9j@protonmail.com>
2024-02-14 14:37:50 -0700

    added five day forecast api. Created a function to grab specific data from list

--- assets/script.js
+++ assets/script.js
@@ -25,6 +25,26 @@ function getWeather(lat, lon) {
             todayWeather(data);
         })
 }
+/*creating function to grab five day forecast from an api URL
+returning digestible object information. */
+function getFiveDayForecast(lat, lon) {
+    var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=38438435bd1e08c9d78e0ac7cd864567&units=imperial`;
+
+    fetch(weatherURL)
+        .then(function (response) {
+            return response.json();
+        })
+        // .then points to that json data, the function calls upon that data or information, console.log prints it so we can view it
+        .then(function (data) {
+            console.log("This here b tha data", data);
+            /*We create a new variable called 'days'. We set it equal to a function which gives us the temps of the next five days. We do this by, calling upon the parameter 'data' which is our now neatly compiled JSON list of info, and attach the file path called 'list' which points to the location of the specific data we're looking for. There are 40 indexes within the 'list' location. We use the '.filter' method to filter the list of 40 indexes and return to us only the indexes that '.includes' the next five day forecast at '9:00:00'. */
+            const days = data.list.filter((day) => day.dt_txt.includes('09:00:00'));
+            /*const days = data.list.filter(function(day) {
+                day.dt_txt.includes('9:00:00');
+            })*/
+            console.log(days);
+        })
+}
 
 const button = document.getElementById("submit-button");
 const searchBox = document.getElementById("search-box");
@@ -46,7 +66,6 @@ searchBox.addEventListener("keydown", function (event) {
 function getLatAndLong(cityName) {
     console.log(cityName);
     const cityNameURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=38438435bd1e08c9d78e0ac7cd864567'
-    console.log(cityNameURL);
     fetch(cityNameURL)
         .then(function (response) {
             console.log(response);
@@ -57,6 +76,7 @@ function getLatAndLong(cityName) {
             const lat = data[0].lat
             const lon = data[0].lon
             getWeather(lat, lon);
+            getFiveDayForecast(lat, lon);
         })
 }
 var currentWeather = document.querySelector("#current-weather");
@@ -72,12 +92,13 @@ function todayWeather(data) {
     currentTimeLabel.textContent = `Temp : `;
     currentTimeLabel.style.color = "#ff2a6d";
     var currentTempSpan = document.createElement("span");
-    currentTempSpan.textContent = data.main.temp;
-    currentTempSpan.style.color = "#05d9e8";
-    var currentTimeDegree = document.createElement("span")
-    currentTimeDegree.textContent = ` °`;
+    currentTempSpan.textContent = `${data.main.temp}  °`;
+    // currentTempSpan.style.color = "#05d9e8";
+    currentTempSpan.setAttribute("class", "cyan-text");
+    // var currentTimeDegree = document.createElement("span")
+    // currentTimeDegree.textContent = ` °`;
     // currentTemp.innerHTML = `Temp:   <span  class="temp-color-style">${data.main.temp}</span> °`
-    currentTemp.append(currentTimeLabel, currentTempSpan, currentTimeDegree)
+    currentTemp.append(currentTimeLabel, currentTempSpan)
 
     var currentWind = document.createElement("p");
     //this line is gathering data from the api object in the console using the '.name' as a selector
@@ -94,6 +115,7 @@ function todayWeather(data) {
 
 }
 
+// erases current weather search so that the function can run and populate the next weather search
 function clearSubmition() {
     currentWeather.innerHTML = "";
 }
